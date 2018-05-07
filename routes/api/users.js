5fdd5cf76f05ceb0652bda1940e0a1d1382dbf23
Vick Kumar <vick.kumar44@gmail.com>
2018-05-07 13:40:41 -0400

    added register and login post path

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,5 @@
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
 
@@ -14,24 +15,60 @@ router.get('/test', (req, res) => res.json({msg: "Users Works"}))
 // @desc    Register User
 // @access  Public
 router.post('/register', (req, res) => {
-  User.findOne({ email: req.body.email})
-  .then(user => {
-    if(user) {
-      return res.status(400).json({email: 'Email already exists'})
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      
     } else {
       const avatar = gravatar.url(req.body.email, {
-        s: '200', //Size
-        r: 'pg', //Rating
-        d: 'mm' //Default
-      })
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default
+      });
 
-      const newUser = new user({
+      const newUser = new User({
         name: req.body.name,
         email: req.body.email,
         avatar,
         password: req.body.password
-      })
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
     }
+  });
+});
+
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({email})
+    .then(user => {
+      // Check for user
+      if(!user) {
+        return res.status(404).json({email: "User does not exist"});
+      }
+      // Check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch) {
+            res.json({meg: 'Success'});
+          } else {
+            res.status(400).json({password: 'Password incorrect'});
+          }
+        }) 
   })
 })
 
